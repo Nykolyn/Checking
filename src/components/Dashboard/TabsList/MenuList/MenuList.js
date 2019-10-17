504f84678c110367c25ffc10264f9408ad089a86
BohdanRadchenko <radchenkobs@gmail.com>
2019-10-17 17:17:48 +0300

    added loadable and loader/  Async Tab

--- src/components/Dashboard/TabsList/MenuList/MenuList.js
+++ src/components/Dashboard/TabsList/MenuList/MenuList.js
@@ -1,9 +1,43 @@
 import React from 'react';
 import { NavLink, Route, Redirect, Switch } from 'react-router-dom';
-import TodayTab from '../TodayTomorrowTab/TodayTomorrowTab';
-import NextWeekTab from '../NexWeekTab/NexWeekTab';
-import BurnedOutTab from '../BurnedOutTab/BurnedOutTab';
-import DoneTab from '../DoneTab/DoneTab';
+import Loadable from 'react-loadable';
+import Loader from '../Loader';
+
+const AsyncTodayTab = Loadable({
+  loader: () =>
+    import(
+      '../TodayTomorrowTab/TodayTomorrowTab' /* webpackChunkName: "TodayTomorrow-page" */
+    ),
+  loading: Loader,
+  timeout: 10000,
+  delay: 200,
+});
+
+const AsyncNextWeekTab = Loadable({
+  loader: () =>
+    import('../NexWeekTab/NexWeekTab' /* webpackChunkName: "NexWeek-page" */),
+  loading: Loader,
+  timeout: 10000,
+  delay: 200,
+});
+
+const AsyncBurnedOutTab = Loadable({
+  loader: () =>
+    import(
+      '../BurnedOutTab/BurnedOutTab' /* webpackChunkName: "BurnedOutTab-page" */
+    ),
+  loading: Loader,
+  timeout: 10000,
+  delay: 200,
+});
+
+const AsyncDoneTab = Loadable({
+  loader: () =>
+    import('../DoneTab/DoneTab' /* webpackChunkName: "DoneTab-page" */),
+  loading: Loader,
+  timeout: 10000,
+  delay: 200,
+});
 
 const MenuList = () => {
   return (
@@ -23,10 +57,10 @@ const MenuList = () => {
       </NavLink>
 
       <Switch>
-        <Route path="/dashboard/today" component={TodayTab} />
-        <Route path="/dashboard/nextweek" component={NextWeekTab} />
-        <Route path="/dashboard/burnedout" component={BurnedOutTab} />
-        <Route path="/dashboard/done" component={DoneTab} />
+        <Route path="/dashboard/today" component={AsyncTodayTab} />
+        <Route path="/dashboard/nextweek" component={AsyncNextWeekTab} />
+        <Route path="/dashboard/burnedout" component={AsyncBurnedOutTab} />
+        <Route path="/dashboard/done" component={AsyncDoneTab} />
         <Redirect to="/dashboard/today" />
       </Switch>
     </div>
