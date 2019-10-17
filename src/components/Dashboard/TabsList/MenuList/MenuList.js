7016f7b8ebb530683611e1a8f3f2225e3dc8a7cc
BohdanRadchenko <radchenkobs@gmail.com>
2019-10-17 12:21:15 +0300

    create component

---
+++ src/components/Dashboard/TabsList/MenuList/MenuList.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const MenuList = () => {
+  <div>
+    <h2>work MenuList</h2>
+  </div>;
+};
---
+++ src/components/Dashboard/TabsList/TabsList.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import MenuList from './MenuList/MenuList';
+
+const TabsList = () => (
+  <div>
+    <h1>work TabsList</h1>
+    <MunuList></MunuList>
+  </div>
+);
+
+export default TabsList;
