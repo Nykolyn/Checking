85bd6d185af051e21f7db252218e6c5b2a710ed7
Roman Bezpalyi <romabespaly@gmail.com>
2019-10-18 12:29:06 +0300

    cloned dev

---
+++ src/components/TaskPopUp/TaskPopUp.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import DateSelect from './DateSelect';
+import RoleSelect from './RoleSelect';
+import TimeSelect from './TimeSelect';
+import PrioritySelect from './PrioritySelect';
+import styles from './TaskPopUp.module.css';
+
+export default class TaskPopUp extends Component {
+  state = {
+    role: 'None',
+  };
+
+  render() {
+    const { role } = this.state;
+    return (
+      <section className={styles.outer}>
+        <h3 className={styles.createTaskTitle}>Create Task</h3>
+        <div className={styles.helperDiv}>
+          <RoleSelect />
+          <DateSelect />
+        </div>
+        <h4 className={styles.titleTitle}>Title (up to 150 characters)</h4>
+        <input className={styles.titleInput} type="text" required />
+        <h4 className={styles.title}>Description (up to 800 characters)</h4>
+        <textarea className={styles.textarea} rows={7} />
+        <div className={styles.flexHelperDiv}>
+          <TimeSelect />
+          <PrioritySelect />
+        </div>
+        <div className={styles.btnDiv}>
+          <button type="button" className={styles.btn}>
+            Cancel
+          </button>
+          <button type="button" className={styles.acceptBtn}>
+            Accept
+          </button>
+        </div>
+      </section>
+    );
+  }
+}
---
+++ src/helpers/timeSelectStylesHelper.js
@@ -0,0 +1,43 @@
+const timeStyles = {
+  control: styles => ({
+    ...styles,
+    width: '100%',
+    padding: 0,
+    borderRadius: 0,
+    borderWidth: '1px 0 0 0',
+    borderStyle: 'solid',
+    borderColor: '#f1f1f1',
+  }),
+  valueContainer: styles => {
+    return {
+      ...styles,
+      height: '40px',
+      backgroundColor: '#f5f7fa',
+    };
+  },
+  indicatorSeparator: styles => ({
+    ...styles,
+    display: 'none',
+  }),
+  indicatorsContainer: styles => ({
+    ...styles,
+    height: '40px',
+    backgroundColor: '#f5f7fa',
+    fill: '#000',
+  }),
+  menu: styles => ({ ...styles, borderRadius: 0, margin: 0 }),
+  option: styles => ({
+    ...styles,
+    boxSizing: 'border-box',
+    border: '1px solid #f5f7fa',
+    backgroundColor: '#fff',
+    color: 'black',
+
+    ':active': {
+      ...styles[':active'],
+      fontWeight: 'bold',
+    },
+  }),
+};
+
+export default timeStyles;
