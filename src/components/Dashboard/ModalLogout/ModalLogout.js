2581f52263324d149593269bae9d7bf50183fb0a
Denys Sobol <ratnev@gmail.com>
2019-10-18 15:11:28 +0300

    new component structure

--- src/components/Dashboard/ModalLogout/ModalLogout.js
+++ src/components/Dashboard/ModalLogout/ModalLogout.js
@@ -1,10 +1,43 @@
-import React from 'react';
-// import Dashboard from '../Dashboard';
+/*eslint-disable*/
+import React, { Component, createRef } from 'react';
+import styles from './ModalLogout.module.css';
 
-const ModalLogout = () => (
-  <div>
-    <p>Are you sure you want to logout?!</p>
-  </div>
-);
+export default class Modal extends Component {
+  state = {};
 
-export default ModalLogout;
+  backdropRef = createRef();
+
+  componentDidMount() {
+    window.addEventListener('keydown', this.handleKeyPress);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('keydown', this.handleKeyPress);
+  }
+
+  handleKeyPress = e => {
+    if (e.code !== 'Escape') return;
+    this.props.onClose();
+  };
+
+  handleBackdropClick = e => {
+    const { current } = this.backdropRef;
+    if (current && e.target !== current) {
+      return;
+    }
+    this.props.onClose();
+  };
+
+  render() {
+    const { children, url } = this.props;
+    return (
+      <div
+        className={styles.backdrop}
+        ref={this.backdropRef}
+        onClick={this.handleBackdropClick}
+      >
+        <div className={styles.modal}>{children}</div>
+      </div>
+    );
+  }
+}
