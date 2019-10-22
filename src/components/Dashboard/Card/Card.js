d412fc61e663ad63a2a60b73f500ca35623576f5
RZFalcore <box.rz.box@gmail.com>
2019-10-22 16:05:42 +0300

    resolving role drowning

--- src/components/Dashboard/Card/Card.js
+++ src/components/Dashboard/Card/Card.js
@@ -5,43 +5,45 @@ import style from './Card.module.css';
 
 class Card extends Component {
   state = {
+    propsRole: 'Learner',
     title: 'Title with many - many symbols, many - many realy',
     text:
       'Body text(60 symb) Body text(60 symb) Body text(60 symb) Body text(60 symb) Body text(60 symb) Body text(60 symb)',
   };
 
   render() {
-    const { title, text } = this.state;
+    const { propsRole, title, text } = this.state;
     // console.log(title.length, text.length);
-
+    let role;
+    if (propsRole === 'Learner') {
+      role = <p className={style.cardHeader__roleLearner}> Learner </p>;
+    }
     return (
       <section className={style.cardSection}>
         <header className={style.cardHeaderNone}>
-          <p className={style.cardHeader__role}> Role </p>{' '}
-          <p className={style.cardHeader__priorityOne}> 1 </p>{' '}
-        </header>{' '}
+          {/* <p className={style.cardHeader__role}> Role </p> */}
+          {role}
+          <p className={style.cardHeader__priorityOne}> 1 </p>
+        </header>
         <div className={style.cardBody}>
           <p className={style.cardBody__title}>
-            {' '}
-            {title.length > 46 ? `${title.trim().slice(0, 45)}...` : title}{' '}
-          </p>{' '}
+            {title.length > 46 ? `${title.trim().slice(0, 45)}...` : title}
+          </p>
           <p className={style.cardBody__text}>
-            {' '}
-            {text.length > 110 ? `${text.trim().slice(0, 100)}...` : text}{' '}
-          </p>{' '}
-        </div>{' '}
+            {text.length > 110 ? `${text.trim().slice(0, 109)}...` : text}
+          </p>
+        </div>
         <footer className={style.cardFooter}>
           <div className={style.cardFooter__wrap}>
             <p className={style.cardFooter__dateTime}>
-              {' '}
-              Oct 10, 2020 | 06.00 - 09.00{' '}
-            </p>{' '}
-          </div>{' '}
+              Oct 10, 2020 | 06.00 - 09.00
+            </p>
+          </div>
           <div>
             <EditButton />
             <DoneButton />
-          </div>{' '}
-        </footer>{' '}
+          </div>
+        </footer>
       </section>
     );
   }
