1ca7a8dadd74ac29b46e4c5951d3b994528d46b3
AntonMuzychenko <49153700+AntonMuzychenko@users.noreply.github.com>
2019-10-23 11:14:09 +0300

    commit

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
-// import TabsList from './TabsList/TabsList';
+import TabsList from './TabsList/TabsList';
 import Header from './Header/Header';
-// import TaskPopUp from '../TaskPopUp/TaskPopUp';
+import TaskPopUp from '../TaskPopUp/TaskPopUp';
 
 class Dashboard extends Component {
   componentDidMount() {}
@@ -11,8 +11,8 @@ class Dashboard extends Component {
       <main>
         <Header />
         <h1>Dashboard page</h1>
-        {/* <TabsList /> */}
-        {/* <TaskPopUp /> */}
+        <TabsList />
+        <TaskPopUp />
       </main>
     );
   }
