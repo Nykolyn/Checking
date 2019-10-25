3dd92d4f4e3d6d54a2e671ed6faeabc1b3aee027
NikitaDiver <NikitaDiver@users.noreply.github.com>
2019-10-25 17:35:54 +0300

    add header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,43 +1,46 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { connect } from 'react-redux';
 import styles from './Header.module.css';
 import { burgerMenuOpen } from '../../redux/componentController/componentActions';
 import { burgerMenuIsOpen } from '../../redux/componentController/controllerSelectrors';
 import { ReactComponent as Logo } from '../../assets/icons/logo-darkblue.svg';
 
-const Header = ({ isOpen, burgerMenuToogle }) => <header>
+const Header = ({ isOpen, burgerMenuToogle }) => (
+  <header>
     <nav>
-        <ul className={styles.container}>
-            <li className={styles.li}>
-                <button click="onClick" className={styles.donutBtn} />
-            </li>
-            <li className={styles.li}>
-                <Logo className={styles.logo} />
-            </li>
-            <li className={styles.li}>
-                <button
-                    onClick={burgerMenuToogle}
-                    className={styles.logOut}
-                    // className={
-                    //     isOpen
-                    //         ? [styles.button, styles.checked].join(' ')
-                    //         : styles.button
-                    // }
-                >Log out</button>
-            </li>
-        </ul>
+      <ul className={styles.container}>
+        <li className={styles.li}>
+          <button click="onClick" className={styles.donutBtn} />
+        </li>
+        <li className={styles.li}>
+          <Logo className={styles.logo} />
+        </li>
+        <li className={styles.li}>
+          <button
+            onClick={burgerMenuToogle}
+            className={styles.logOut}
+            // className={
+            //     isOpen
+            //         ? [styles.button, styles.checked].join(' ')
+            //         : styles.button
+            // }
+          >
+            Log out
+          </button>
+        </li>
+      </ul>
     </nav>
-</header>
-
+  </header>
+);
 
 const mSTP = state => ({
-    isOpen: burgerMenuIsOpen(state),
+  isOpen: burgerMenuIsOpen(state),
 });
 const mDTP = dispatch => ({
-    burgerMenuToogle: () => dispatch(burgerMenuOpen()),
+  burgerMenuToogle: () => dispatch(burgerMenuOpen()),
 });
 
 export default connect(
-    mSTP,
-    mDTP,
+  mSTP,
+  mDTP,
 )(Header);
