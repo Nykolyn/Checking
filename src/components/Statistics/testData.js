f6398422b0b63d582d9efbbce9fb8c0a29a9bbf2
petrovskyi <ganeshka@icloud.com>
2019-10-27 16:38:28 +0200

    update

---
+++ src/components/Statistics/testData.js
@@ -0,0 +1,88 @@
+const a = [
+  {
+    role: 'Co-worker',
+  },
+  {
+    role: 'Co-worker',
+  },
+  {
+    role: 'Co-worker',
+  },
+  {
+    role: 'Co-worker',
+  },
+  {
+    role: 'Co-worker',
+  },
+  {
+    role: 'Co-worker',
+  },
+  {
+    role: 'Co-worker',
+  },
+  {
+    role: 'Co-worker',
+  },
+  {
+    role: 'Co-worker',
+  },
+  {
+    role: 'Co-worker',
+  },
+  {
+    role: 'Co-worker',
+  },
+  {
+    role: 'Co-worker',
+  },
+  {
+    role: 'Partner',
+  },
+  {
+    role: 'Partner',
+  },
+  {
+    role: 'Partner',
+  },
+  {
+    role: 'Partner',
+  },
+  {
+    role: 'Partner',
+  },
+  {
+    role: 'Learner',
+  },
+  {
+    role: 'Learner',
+  },
+  {
+    role: 'Learner',
+  },
+  {
+    role: 'Learner',
+  },
+  {
+    role: 'Learner',
+  },
+  {
+    role: 'Learner',
+  },
+  {
+    role: 'Learner',
+  },
+  {
+    role: 'Learner',
+  },
+  {
+    role: 'Learner',
+  },
+  {
+    role: 'Learner',
+  },
+  {
+    role: 'Learner',
+  },
+];
+
+export default a;
