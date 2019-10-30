ae114772efbfd7d8e72e725a3da7779c4ba811aa
maryavasylieva <miheta.laurelin@gmail.com>
2019-10-30 10:41:26 +0200

    validation

--- src/components/Authentification/LogInForm/LogIn.js
+++ src/components/Authentification/LogInForm/LogIn.js
@@ -1,37 +1,53 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
-// import ReactRouterPropTypes from 'react-router-prop-types';
+import ReactRouterPropTypes from 'react-router-prop-types';
 import { Link } from 'react-router-dom';
-import { Formik } from 'formik';
 import style from './LogInForm.module.css';
 import FormButton from '../FormButton';
 import Header from '../../Header/Header';
-import { signIn } from '../../../redux/session/sessionOperations';
+// import { signIn } from '../../../redux/session/sessionOperations';
 import { isAuthentificated } from '../../../redux/session/sessionSelectors';
 
 class LogIn extends Component {
-  state = {
-    email: '',
-    password: '',
+  static propTypes = {
+    authentificated: PropTypes.bool.isRequired,
+    history: ReactRouterPropTypes.history.isRequired,
   };
+  // state = {
+  //   email: '',
+  //   password: '',
+  // };
 
-  handleSubmit = e => {
-    e.preventDefault();
+  componentDidUpdate() {
+    const { authentificated, history } = this.props;
+    if (authentificated) {
+      history.replace('/dashboard');
+    }
+  }
+  // handleSubmit = e => {
+  //   e.preventDefault();
 
-    this.props.onLogin({ ...this.state });
-    this.setState({ email: '', password: '' });
-  };
+  //   this.props.onLogin({ ...this.state });
+  //   this.setState({ email: '', password: '' });
+  // };
 
-  handleChange = e => {
-    this.setState({
-      [e.target.name]: e.target.value,
-    });
-  };
+  // handleChange = e => {
+  //   this.setState({
+  //     [e.target.name]: e.target.value,
+  //   });
+  // };
 
   render() {
     const screenWidth = document.documentElement.clientWidth;
-    const { email, password } = this.state;
+    const {
+      values,
+      handleBlur,
+      handleChange,
+      handleSubmit,
+      errors,
+      touched,
+    } = this.props;
     return (
       <div className={style.wrapLogin}>
         {screenWidth < 768 && <Header />}
@@ -54,18 +70,22 @@ class LogIn extends Component {
                 <span className={style.sloganText}>Just check in.</span>
               </div>
 
-              <form className={style.formLogin} onSubmit={this.handleSubmit}>
+              <form className={style.formLogin} onSubmit={handleSubmit}>
                 <p className={style.textEmail}>
                   E-mail<span className={style.star}>*</span>
                 </p>
                 <input
-                  onChange={this.handleChange}
-                  value={email}
+                  value={values.email}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
                   type="email"
                   name="email"
                   className={style.input}
                   placeholder="your@email.com"
                 />
+                {errors.email && touched.email && (
+                  <div className={style.inputError}>{errors.email}</div>
+                )}
                 <p className={style.password}>
                   <span className={style.textPassword}>
                     Password<span className={style.star}>*</span>
@@ -75,13 +95,17 @@ class LogIn extends Component {
                   </Link>
                 </p>
                 <input
-                  onChange={this.handleChange}
-                  value={password}
+                  value={values.password}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
                   type="password"
                   name="password"
                   className={style.input}
                   placeholder="yourpassword"
                 />
+                {errors.email && touched.email && (
+                  <div className={style.inputError}>{errors.email}</div>
+                )}
 
                 <FormButton type="submit">Log In</FormButton>
               </form>
@@ -94,7 +118,13 @@ class LogIn extends Component {
 }
 
 LogIn.propTypes = {
-  onLogin: PropTypes.func.isRequired,
+  // onLogin: PropTypes.func.isRequired,
+  handleBlur: PropTypes.func.isRequired,
+  handleChange: PropTypes.func.isRequired,
+  handleSubmit: PropTypes.func.isRequired,
+  values: PropTypes.objectOf(PropTypes.string).isRequired,
+  errors: PropTypes.objectOf(PropTypes.string).isRequired,
+  touched: PropTypes.objectOf(PropTypes.any).isRequired,
 };
 
 const mapStateToProps = state => ({
@@ -102,7 +132,7 @@ const mapStateToProps = state => ({
 });
 
 const mapDispatchToProps = {
-  onLogin: signIn,
+  // onLogin: signIn,
 };
 
 export default connect(
--- src/pages/AuthPage/LoginPage.js
+++ src/pages/AuthPage/LoginPage.js
@@ -2,24 +2,48 @@ import React from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import ReactRouterPropTypes from 'react-router-prop-types';
+import { Formik } from 'formik';
+// import * as Yup from 'yup';
 import LogIn from '../../components/Authentification/LogInForm/LogIn';
-import { isAuthentificated } from '../../redux/session/sessionSelectors';
+// import { isAuthentificated } from '../../redux/session/sessionSelectors';
 import authRedirect from '../../components/Authentification/AuthRedirect';
+import { signIn } from '../../redux/session/sessionOperations';
 
-const LoginPage = ({ history, authentificated }) => (
-  <LogIn history={history} authentificated={authentificated} />
+const LoginPage = ({ history, onSignIn }) => (
+  <div>
+    <Formik
+      initialValues={{
+        email: '',
+        password: '',
+      }}
+      onSubmit={(values, { setSubmitting }) => {
+        setTimeout(() => {
+          const { email, password } = values;
+          onSignIn({ email, password });
+          setSubmitting(false);
+        }, 100);
+      }}
+    >
+      {props => <LogIn {...props} history={history} />}
+    </Formik>
+  </div>
 );
 
 LoginPage.propTypes = {
   history: ReactRouterPropTypes.history.isRequired,
-  authentificated: PropTypes.bool.isRequired,
+  // authentificated: PropTypes.bool.isRequired,
+  onSignIn: PropTypes.func.isRequired,
 };
 
-const mapStateToProps = state => ({
-  authentificated: isAuthentificated(state),
-});
+// const mapStateToProps = state => ({
+//   authentificated: isAuthentificated(state),
+// });
+
+const mapDispatchToProps = {
+  onSignIn: signIn,
+};
 
 export default connect(
-  mapStateToProps,
-  null,
+  // mapStateToProps,
+  mapDispatchToProps,
 )(authRedirect(LoginPage));
