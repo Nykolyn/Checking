1ff621181c5cd7792254163597e0e2f9eec38b03
Denys Sobol <ratnev@gmail.com>
2019-10-30 15:05:31 +0200

    complete

--- src/components/Dashboard/ModalLogout/ModalLogout.js
+++ src/components/Dashboard/ModalLogout/ModalLogout.js
@@ -3,11 +3,8 @@ import React, { Component, createRef } from 'react';
 import { connect } from 'react-redux';
 import styles from './ModalLogout.module.css';
 import { modalLogoutClose } from '../../../redux/componentController/componentActions';
+import { signOut } from '../../../redux/session/sessionOperations';
 class ModalLogout extends Component {
-  // add state from Reduxz
-
-  // ---#---
-
   backdropRef = createRef();
 
   componentDidMount() {
@@ -20,6 +17,10 @@ class ModalLogout extends Component {
 
   onClose = () => {
     return this.props.modalLogoutClose();
+  };
+
+  onSignOut = () => {
+    return this.props.signOut();
   };
 
   handleKeyPress = e => {
@@ -37,16 +38,35 @@ class ModalLogout extends Component {
   };
 
   render() {
-    const { children } = this.props;
-
     return (
-      this.props.openModal && (
+      this.props.openModalLogout && (
         <div
           className={styles.backdrop}
           ref={this.backdropRef}
           onClick={this.handleBackdropClick}
         >
-          <div className={styles.modal}>{children}</div>
+          <div className={styles.modal}>
+            <p className={styles.question}>Are you sure you want to logout?</p>
+            <div className={styles.buttons}>
+              <button
+                type="button"
+                className={styles.button}
+                onClick={() => {
+                  this.onSignOut();
+                  this.onClose();
+                }}
+              >
+                Yes
+              </button>
+              <button
+                type="button"
+                className={styles.noButton}
+                onClick={this.onClose}
+              >
+                No
+              </button>
+            </div>
+          </div>
         </div>
       )
     );
@@ -54,10 +74,10 @@ class ModalLogout extends Component {
 }
 
 const mapStateToProps = state => ({
-  openModal: state.componentController.modalLogoutOpen,
+  openModalLogout: state.componentController.modalLogoutOpen,
 });
 
-const mapDispatchToProps = { modalLogoutClose };
+const mapDispatchToProps = { modalLogoutClose, signOut };
 
 export default connect(
   mapStateToProps,
