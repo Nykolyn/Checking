de004ff53ff56d6f5d899750a03442c424480082
Roman Bezpalyi <49277447+RomanBezpalyi@users.noreply.github.com>
2019-11-05 12:20:02 +0200

    Update Chart.js

--- src/components/Statistics/Chart/Chart.js
+++ src/components/Statistics/Chart/Chart.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Doughnut } from 'react-chartjs-2';
-import test from '../test';
 
 const filteredData = array => {
   const partner = array.filter(el => el.role === 'Partner').length;
