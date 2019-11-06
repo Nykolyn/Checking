bc2cea82625a842e98de010cf73f48b0fef5e65e
RZFalcore <box.rz.box@gmail.com>
2019-11-06 11:26:30 +0200

    before megre

--- src/components/Statistics/Select/Select.js
+++ src/components/Statistics/Select/Select.js
@@ -23,6 +23,7 @@ const customStyles = {
   }),
   container: base => ({
     fontSize: 20,
+    textAlign: 'center',
     position: 'absolute',
     width: '25%',
     height: '100%',
@@ -44,6 +45,7 @@ const customStyles = {
   singleValue: () => ({
     fontSize: 26,
     fontWeight: 500,
+    textAlign: 'center',
     color: '#869096',
     width: '50%',
   }),
